e6016dbe527d4f15d0c03699f1e93508c67d4628
Tetiana.Malva <malva777@ukr.net>
2016-10-09 16:17:54 +0200

    added my fist program

---
+++ src/main/java/com/tanya/test/Program1.java
@@ -0,0 +1,11 @@
+package com.tanya.test;
+
+/**
+ * This is my first Java program <br><br/>
+ * Created by tanya on 10/9/16.
+ */
+public class Program1 {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
